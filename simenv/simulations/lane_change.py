b87e0e133d6c0df32f8fd6f1f14bc26024c88b08
Khaled Ghobashy <khaled.ghobashy@live.com>
2020-04-01 12:09:50 +0200

    updated model to match updated symbolic API

--- simenv/simulations/lane_change.py
+++ simenv/simulations/lane_change.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from uraeus.nmbd.python import simulation
 from uraeus.nmbd.python.engine.numerics.math_funcs import A
@@ -10,7 +11,7 @@ from uraeus.nmbd.python.engine.numerics.math_funcs import A
 database_directory = os.path.abspath('../../')
 sys.path.append(database_directory)
 
-from uraeus_fsae.simenv.assemblies import asurt_FS17 as num_assm
+from uraeus_fsae.simenv.assemblies import asurt_FS17_v1 as num_assm
 
 num_model = num_assm.num_model
 
@@ -67,10 +68,8 @@ num_assm.terrain_data.get_state = terrain_state
 
 num_assm.ST1_config.UF_mcs_rack_act = steering_function
 
-num_assm.AX1_config.UF_far_drive = FR_Torque
-num_assm.AX1_config.UF_fal_drive = FL_Torque
-num_assm.AX2_config.UF_far_drive = RR_Torque
-num_assm.AX2_config.UF_fal_drive = RL_Torque
+num_assm.DR2_config.UF_far_drive = RR_Torque
+num_assm.DR2_config.UF_fal_drive = RL_Torque
 
 num_assm.CH_config.UF_fas_aero_drag_F = zero_func
 num_assm.CH_config.UF_fas_aero_drag_T = zero_func
@@ -80,7 +79,6 @@ num_assm.CH_config.UF_fas_aero_drag_T = zero_func
 sim = simulation('sim', num_model, 'dds')
 sim.set_time_array(9, dt)
 
-import matplotlib.pyplot as plt
 plt.plot(sim.soln.time_array, [steering_function(i) for i in sim.soln.time_array])
 plt.show()
 
@@ -88,8 +86,8 @@ sim.set_initial_states('results/equilibrium_v4.npz')
 
 sim.solve()
 
-sim.save_as_csv('results', 'lanechange_v2', 'pos')
-sim.save_as_npz('results', 'lanechange_v2')
+sim.save_as_csv('results', 'lanechange_v4', 'pos')
+sim.save_as_npz('results', 'lanechange_v4')
 
 
 #=============================================================================
@@ -100,6 +98,8 @@ import matplotlib.pyplot as plt
 
 sim.soln.pos_dataframe.plot(x='CH.rbs_chassis.x', y='CH.rbs_chassis.y', grid=True)
 
+sim.soln.vel_dataframe.plot(x='time', y='CH.rbs_chassis.x', grid=True)
+
 sim.soln.pos_dataframe.plot(x='time', y='CH.rbs_chassis.z', grid=True)
 sim.soln.vel_dataframe.plot(x='time', y='CH.rbs_chassis.z', grid=True)
 sim.soln.acc_dataframe.plot(x='time', y='CH.rbs_chassis.z', grid=True)
@@ -109,27 +109,4 @@ sim.soln.pos_dataframe.plot(x='time', y='CH.rbs_chassis.e1', grid=True)
 sim.soln.pos_dataframe.plot(x='time', y='CH.rbs_chassis.e2', grid=True)
 sim.soln.pos_dataframe.plot(x='time', y='CH.rbs_chassis.e3', grid=True)
 
-
-sim.soln.pos_dataframe.plot(x='time', 
-                            y=['AX1.rbr_hub.z', 'AX1.rbl_hub.z',
-                               'AX2.rbr_hub.z', 'AX2.rbl_hub.z'], 
-                            grid=True)
-
-sim.soln.pos_dataframe.plot(x='time', 
-                            y=['AX1.rbr_hub.x', 'AX1.rbl_hub.x',
-                               'AX2.rbr_hub.x', 'AX2.rbl_hub.x'], 
-                            grid=True)
-
-sim.soln.vel_dataframe.plot(x='time', 
-                            y=['AX1.rbr_hub.x', 'AX1.rbl_hub.x',
-                               'AX2.rbr_hub.x', 'AX2.rbl_hub.x'], 
-                            grid=True)
-
-sim.soln.pos_dataframe.plot(x='time', 
-                            y=['AX1.rbr_hub.e0', 'AX1.rbr_hub.e1',
-                               'AX1.rbr_hub.e2', 'AX1.rbr_hub.e3'], 
-                            grid=True)
-
 plt.show()
-
-
